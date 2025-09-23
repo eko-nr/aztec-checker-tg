7d7f2af8b4ea22125eac383e954d2dd6e5d8316c
eko-nr <eko.nr@yahoo.com>
2025-09-23 19:07:28 +0700

    chore: oke

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ bot.command("show_validators", showValidatorsCommand);
 bot.command("epoch", epochCommand);
 bot.command("get_epoch_validator", getEpochValidatorCommand);
 bot.command("broadcast", broadCastMessageCommand);
-bot.command("list_validator", (ctx) => listValidatorCommand(ctx));
+bot.command("list_validator", (ctx) => listValidatorCommand(ctx, false));
 
 // handler
 bot.on("message:text", messageHandler);
